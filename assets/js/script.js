e306a1c24644e68c23b954d7735bfd75d6d529af
iamShafi <67453945+IamShafi@users.noreply.github.com>
2024-06-19 18:18:00 +0600

    fixed filter

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,7 +74,7 @@ overlay.addEventListener("click", testimonialsModalFunc);
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-select-value]");
+const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 console.log(selectItems)
