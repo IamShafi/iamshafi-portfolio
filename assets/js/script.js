9b6a6f9866f0eca15435840c38292003b94076eb
iamShafi <67453945+IamShafi@users.noreply.github.com>
2024-06-19 16:49:00 +0600

    update card ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,8 +74,10 @@ overlay.addEventListener("click", testimonialsModalFunc);
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
+const selectValue = document.querySelector("[data-select-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+console.log(selectItems)
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
 
